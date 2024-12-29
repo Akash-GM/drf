e2d4389c290101a5630718f7fe6f6c6b974eb82c
unknown <george.akash99@gmail.com>
2024-12-29 15:40:17 +0530

    added permissions ,django model permissions ,registered the product in admin panel ,will add custom perms before next commit; added the update api view

--- backend/cfehome/products/urls.py
+++ backend/cfehome/products/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns= [
-    path('',views.ProductListCreateAPIView.as_view()),
-    path('<int:pk>',views.ProductDetailAPIView.as_view())
+urlpatterns = [
+    path("", views.ProductListCreateAPIView.as_view()),
+    path("<int:pk>", views.ProductDetailAPIView.as_view()),
+    path("<int:pk>/update", views.ProductUpdateAPIView.as_view()),
 ]
-
-
-
--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -1,23 +1,25 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, authentication
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Product
 from .serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
 
+
 class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset= Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.DjangoModelPermissions]
 
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
 
-        
         print(serializer.validated_data)
-        title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content')
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content")
 
         if content is None:
-            content= title
+            content = title
 
         serializer.save(content=content)
 
@@ -25,48 +27,57 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 class ProductDetailAPIView(generics.RetrieveAPIView):
 
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer 
+    serializer_class = ProductSerializer
 
 
-#class ProductListAPIView(generics.RetrieveAPIView):
+# class ProductListAPIView(generics.RetrieveAPIView):
 
 #    queryset = Product.objects.all()
-#    serializer_class = ProductSerializer 
+#    serializer_class = ProductSerializer
 
 
-@api_view(['GET','POST'])
-def product_alt_view(request,pk=None,*args,**kwargs):
-    method= request.method
+class ProductUpdateAPIView(generics.UpdateAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+    permission_classes = [permissions.DjangoModelPermissions]
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
 
     if method == "GET":
         if pk != None:
-            obj = get_object_or_404(Product,pk=pk)
-            data = ProductSerializer(obj,many=False).data
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
 
             return Response(data)
-         
 
         queryset = Product.objects.all()
-        data = ProductSerializer(queryset,many=True).data
+        data = ProductSerializer(queryset, many=True).data
         return Response(data)
-        
 
     elif method == "POST":
-            
-        serializer = ProductSerializer(data=request.data)
 
+        serializer = ProductSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
 
-            title = serializer.validated_data.get('title')
-            content = serializer.validated_data.get('content')
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content")
 
             if content is None:
-                content= title
+                content = title
 
             serializer.save(content=content)
-            
-            return Response(serializer.data)
-   
-        # return Response({"invalid":"not good data"},status=400)
 
+            return Response(serializer.data)
+
+        # return Response({"invalid":"not good data"},status=400)
